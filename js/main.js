0c880e9291aafaef1a3bedac92c05a42a8727522
myle0031 <myle0031@algonquinlive.com>
2018-12-10 17:56:27 -0500

    good stuff

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ function switchCarousel(id) {
   document.getElementById("carousel1").classList.remove('active')
   document.getElementById("carousel2").classList.remove('active')
   document.getElementById("carousel3").classList.remove('active')
-  document.getElementById("carousel4").classList.remove('active')
   var temp = document.getElementById("carousel" + id)
   temp.classList.add('active')
 };
